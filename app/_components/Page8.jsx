3dc6c5a3ee3855de8d8afebb0d82b35a54f002ae
Atul Kaple <109103910+AtulKaple@users.noreply.github.com>
2024-09-21 03:23:06 +0530

    Update Page8.jsx

--- app/_components/Page8.jsx
+++ app/_components/Page8.jsx
@@ -23,7 +23,7 @@ const Page8 = () => {
         var tl = gsap.timeline({
           scrollTrigger: {
             trigger: ".home",
-            start: "top -40%",
+            start: "top top",
             markers: true,
             scrub: 1,
             pin: true,
